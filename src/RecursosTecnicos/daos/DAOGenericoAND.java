90e12e82505b94a6b53c25880531096e7b6a1af8
FelipeWin <FelipeWin@FelipeWin-PC>
2015-02-08 21:04:26 -0500

    DAOs genericos con AND generalizado con un contador de ANDs en la sentencia.

--- src/RecursosTecnicos/daos/DAOGenericoAND.java
+++ src/RecursosTecnicos/daos/DAOGenericoAND.java
@@ -18,53 +18,58 @@ import java.sql.SQLException;
  * @author dx
  */
 public class DAOGenericoAND {
-    
+
     public static final String OR_PARAM = "OR";
     public static final String AND_PARAM = "AND";
-    
+
     public DAOGenericoAND() {
     }
-    
+
     public void getUsuario(Usuario u, String param, ConexionDb c) {
         String sentencia = "SELECT * FROM usuario u WHERE";
-        
+
         try {
             Connection conexion = c.getConnection();
             int numAtrib = 1;
-            
+            int cont = 0;
+
             if (u.getCodigo() != 0) {
                 sentencia += " codigo = ? ";
+                cont++;
             }
-            
+
             if (u.getNombre() != null && !u.getNombre().equals("")) {
-                if (sentencia.contains("codigo")) {
+                if (cont > 0) {
                     sentencia += "AND nombre = ? ";
                 } else {
                     sentencia += " nombre = ? ";
+                    cont++;
                 }
             }
             if (u.getApellido() != null && !u.getApellido().equals("")) {
-                if (sentencia.contains("codigo") || sentencia.contains("nombre")) {
+                if (cont > 0) {
                     sentencia += "AND apellido = ? ";
                 } else {
                     sentencia += " apellido = ? ";
+                    cont++;
                 }
             }
             if (u.getRol() != 0) {
-                if (sentencia.contains("codigo") || sentencia.contains("nombre") || sentencia.contains("apellido")) {
+                if (cont > 0) {
                     sentencia += "AND rol = ? ";
                 } else {
                     sentencia += " rol = ? ";
+                    cont++;
                 }
             }
-            
+
             PreparedStatement pstm = conexion.prepareStatement(sentencia);
-            
+
             if (u.getCodigo() != 0) {
                 pstm.setInt(numAtrib, u.getCodigo());
                 numAtrib++;
             }
-            
+
             if (u.getNombre() != null && !u.getNombre().equals("")) {
                 pstm.setString(numAtrib, u.getNombre());
                 numAtrib++;
@@ -77,7 +82,7 @@ public class DAOGenericoAND {
                 pstm.setInt(numAtrib, u.getRol());
                 numAtrib++;
             }
-            
+
             System.out.println("consulta :" + pstm);
             ResultSet res = pstm.executeQuery();
             while (res.next()) {
@@ -88,21 +93,21 @@ public class DAOGenericoAND {
                 System.out.println("Pass: " + res.getString("passwd"));
             }
             pstm.close();
-            
+
         } catch (SQLException ex) {
             System.out.println(ex);
         }
     }
-    
+
     public static void main(String[] args) throws IOException {
         Usuario u = new Usuario();
         u.setCodigo(1);
-//        u.setNombre("mauricio");
-        u.setRol(1);
+        u.setNombre("mauricio");
+//        u.setRol(1);
         DAOGenericoAND dao = new DAOGenericoAND();
         ConexionDb con = new ConexionDb();
         dao.getUsuario(u, null, con);
-        
+
     }
-    
+
 }
